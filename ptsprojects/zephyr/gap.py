7933258aa9813d2f20296e114b655360d756ed98
Michał Narajowski <michal.narajowski@codecoup.pl>
2018-12-07 17:21:40 +0100

    GAP: Set IUT IRK

--- ptsprojects/zephyr/gap.py
+++ ptsprojects/zephyr/gap.py
@@ -118,6 +118,9 @@ def test_cases(pts):
                              "GAP", "TSPX_bd_addr_iut",
                              stack.gap.iut_addr_get_str())),
                     TestFunc(lambda: pts.update_pixit_param(
+                        "GAP", "TSPX_iut_device_IRK_for_resolvable_privacy_address_generation_procedure",
+                        "11111111111111111111111111111111")),
+                    TestFunc(lambda: pts.update_pixit_param(
                              "GAP", "TSPX_iut_privacy_enabled",
                              "TRUE" if stack.gap.iut_has_privacy() else "FALSE")),
                     TestFunc(lambda: pts.update_pixit_param(
