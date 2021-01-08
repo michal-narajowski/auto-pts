f40363e6e2738f6a9c2387e3b2c5aefe8c0c05d3
Michał Narajowski <michal.narajowski@codecoup.pl>
2021-01-08 18:41:54 +0100

    mesh: Fix WIDs used in MESH/CFG/HBP/BV-05-C

--- wid/mesh.py
+++ wid/mesh.py
@@ -1246,6 +1246,9 @@ def hdl_wid_333(desc):
                  Please click OK when friendship is terminated.
     :return:
     """
+    stack = get_stack()
+    assert stack.mesh.wait_for_lpn_terminated(timeout=60)
+
     return True
 
 
@@ -1535,7 +1538,9 @@ def hdl_wid_560(desc):
     :param desc: Lower Tester 2 is waiting for IUT's heartbeat triggered by friendship termination.
     :return:
     """
-    btp.mesh_lpn(False)
+    stack = get_stack()
+    assert stack.mesh.wait_for_lpn_terminated(timeout=60)
+
     return True
 
 
@@ -1545,6 +1550,9 @@ def hdl_wid_561(desc):
     :param desc: Lower Tester 2 is waiting for IUT's heartbeat triggered by friendship establishment.
     :return:
     """
+    stack = get_stack()
+    assert stack.mesh.wait_for_lpn_established(timeout=60)
+
     return True
 
 
@@ -1552,15 +1560,6 @@ def hdl_wid_562(desc):
     """
     Implements:
     :param desc: Friendship will be terminated between Lower Tester 1 and IUT. Verifying no heartbeat is triggered by the termination...
-    :return:
-    """
-    return True
-
-
-def hdl_wid_564(desc):
-    """
-    Implements:
-    :param desc: Please wait until Friendship is reestablished between Lower Tester 1 and IUT. Click OK to send Heartbeat Subscription Set message to IUT (Low Power Node).
     :return:
     """
     return True
@@ -1574,6 +1573,20 @@ def hdl_wid_563(desc):
                  to IUT (Low Power Node).
     :return:
     """
+    return True
+
+
+def hdl_wid_564(desc):
+    """
+    Implements:
+    :param desc: Please wait until Friendship is reestablished between Lower Tester
+                 1 and IUT. Click OK to send Heartbeat Subscription Set message
+                 to IUT (Low Power Node).
+    :return:
+    """
+    stack = get_stack()
+    assert stack.mesh.wait_for_lpn_established(timeout=60)
+
     return True
 
 
